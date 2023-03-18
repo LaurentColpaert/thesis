732134cab457977c707975321a522b176af5ee6c
LaurentColpaert <yololoyololo2000@gmail.com>
2023-03-18 21:05:35 +0100

    rastagrin example

--- src/main.py
+++ src/main.py
@@ -1,2 +1,33 @@
-if __name__ == "__main__":
-    print('Hello world')
+from math import *
+import random
+import numpy as np
+from map_elite import MAP_Elite
+
+RANGE = 20
+
+def rastrigin(ind, a=10.):
+    """Rastrigin test function."""
+    n = float(len(ind))
+    return a * n + sum(x * x - a * cos(2. * pi * x) for x in ind),ind
+
+def generate_random():
+    return [random.uniform(-RANGE,RANGE),random.uniform(-RANGE,RANGE)]
+
+def mutate(ind):
+    return [clamp(ind[0] + random.uniform(-1,1),-RANGE,RANGE), clamp(ind[1] + random.uniform(-1,1),-RANGE,RANGE)]
+def mutate_test(ind):
+    return ind + np.random.normal(0, 0.1, len(ind))
+
+def clamp(value, minimum, maximum):
+    return max(minimum, min(value, maximum))
+
+if __name__ == '__main__':
+
+    map_elite = MAP_Elite(b_range=[[-RANGE,RANGE],[-RANGE,RANGE]],width=30,height=30,num_iterations=1000,batch_size=100)
+    map_elite.set_fitness(rastrigin)
+    map_elite.set_mutate(mutate)
+    map_elite.init_archive(10,generate=generate_random)
+    map_elite.map_elite()
+
+    map_elite.display_archive()
+    map_elite.display_progress()
--- src/map_elite.py
+++ src/map_elite.py
@@ -1,29 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 import random
+import math
 
 from species import Species
 
 class MAP_Elite:
-    def __init__(self, num_iterations: int = 50 , batch_size: int = 500) -> None:
+    def __init__(self,b_range,height: int = 20, width: int = 20,  num_iterations: int = 50 , batch_size: int = 500) -> None:
         self.archive = {}
-        self.height = 0
-        self.width = 0
+        self.height = height
+        self.width = width
         self.num_iterations = num_iterations
         self.batch_size = batch_size
+        self.b_range = b_range
 
         #To plot the progress
         self.coverages = []
         self.means = []
 
-    def init_archive(self, size : int):
-        archive = {}
+        #Functions
+        self.fitness_fn = self.fitness
+        self.mutate_fn = self.mutate
+
+
+    def init_archive(self, size : int, generate):
         for _ in range(size):
-            g = self.generate_pfsm()
-            f, b = self.fitness(g)
-            self.add_to_archive(archive, Species(g, b, f))
-        return archive
+            g = generate()
+            f, b = self.fitness_fn(g)
+            self.add_to_archive(Species(g, b, f))
     
     def fitness(self, genotype):
         """
@@ -31,33 +36,48 @@ class MAP_Elite:
         """
         pass
 
-    def mutate(self):
+    def generate_pfsm(self):
+        pass
+
+    def mutate(self,x):
         pass
 
     def map_elite(self):
-        for _ in tqdm(range(self.num_iterations)):
+        for i in tqdm(range(self.num_iterations)):
+            # if i % 100 == 0:
+                # print(f"Step {i}")
             # self.display_archive()
             for _ in range(self.batch_size):
                 # Pick an existing random point in the archive
                 x = random.choice(list(self.archive.values()))
                 # Mutate it
-                g = self.mutate()
+                g = self.mutate_fn(x.genotype)
+                # print("g is ", g)
                 # Compute the fitness and behaviour
-                f,b = self.fitness(g)
+                f,b = self.fitness_fn(g)
 
                 self.add_to_archive(Species(g,b,f))
-        coverage, mean = self.qd_scores()
-        self.coverages += [coverage]
-        self.means += [mean]
+            coverage, mean = self.qd_scores()
+            self.coverages += [coverage]
+            self.means += [mean]
 
 
     def add_to_archive(self, species: Species):
         """
         Map the species behaviour into the archive,
         and store it only if it does not exist or its fitness value is higher than the previous species of the cell.
-        """
-        n = species.behavior * np.array([self.height, self.width])
-        x,y = min(round(n[0]), self.height-1), min(round(n[1]), self.width-1)
+        """        
+
+        if species.behavior[0] != self.b_range[0][1]: 
+            x = math.floor((abs(self.b_range[0][0]) + species.behavior[0]) * (self.width -1)/(abs(self.b_range[0][1]) + abs(self.b_range[0][0]) ))
+        else:
+            x = self.width -1
+        
+        if species.behavior[1] != self.b_range[1][1]:
+            y = math.floor((abs(self.b_range[1][0]) + species.behavior[1]) *(self.height - 1)/(abs(self.b_range[1][1]) + abs(self.b_range[1][0])) )
+        else:
+            y = self.height -1
+
         if (x, y) not in self.archive or self.archive[(x, y)].fitness < species.fitness:
             self.archive[(x,y)] = species
             species.niche = (x,y)
@@ -66,7 +86,7 @@ class MAP_Elite:
         """
         Compute the coverage and the mean fitness of the archive
         """
-        coverage = len(self) / float(self.height * self.width)
+        coverage = len(self.archive) / float(self.height * self.width)
         fit_list = [x.fitness for x in list(self.archive.values())]
         mean = sum(fit_list) / float(self.height * self.width)
         return coverage, mean
@@ -89,7 +109,7 @@ class MAP_Elite:
         fit = np.zeros((self.height, self.width))
         m_min = 1e10
         m_max = 0
-        for ((x,y), s) in self.items():
+        for ((x,y), s) in self.archive.items():
             fit[x,y] = s.fitness
             if s.fitness < m_min:
                 m_min = s.fitness
@@ -97,6 +117,12 @@ class MAP_Elite:
                 m_max = s.fitness
         plt.imshow(np.array(fit))
         plt.clim(m_min, m_max)
-
+        plt.show()
         # fig.canvas.draw()
         return m_min, m_max
+    
+    def set_fitness(self,fitness_fn):
+        self.fitness_fn = fitness_fn
+    
+    def set_mutate(self, mutate_fn):
+        self.mutate_fn = mutate_fn
