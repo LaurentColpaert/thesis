072724fc0b6bbd6594cf4d3469cd5f039117d3cf
LaurentColpaert <yololoyololo2000@gmail.com>
2023-03-23 20:10:14 +0100

    simulation working

--- src/main.py
+++ src/main.py
@@ -18,7 +18,15 @@ def mutate(ind):
 
 def run_argos():
     """
-    [HOME]/AutoMoDe/bin/automode_main/automode_main -n -c [mission].argos [pfsm]
+    Visu :
+/home/laurent/AutoMoDe/bin/automode_main -c aacVisu.argos -n --fsm-config --nstates 4 --s0 4 --att0 3.25 --n0 2 --n0x0 0 --c0x0 5 --p0x0 0.23 --n0x1 2 --c0x1 0 --p0x1 0.70 --s1 2 --n1 3 --n1x0 0 --c1x0 4 --w1x0 8.91 --p1x0 7 --n1x1 1 --c1x1 0 --p1x1 0.15 --n1x2 2 --c1x2 3 --w1x2 1.68 --p1x2 10 --s2 1 --n2 1 --n2x0 0 --c2x0 3 --w2x0 6.93 --p2x0 4 --s3 4 --att3 3.71 --n3 2 --n3x0 0 --c3x0 1 --p3x0 0.50 --n3x1 2 --c3x1 5 --p3x1 0.62
+    No Visu
+/home/laurent/AutoMoDe/bin/automode_main -c aac.argos -n --fsm-config --nstates 4 --s0 4 --att0 3.25 --n0 2 --n0x0 0 --c0x0 5 --p0x0 0.23 --n0x1 2 --c0x1 0 --p0x1 0.70 --s1 2 --n1 3 --n1x0 0 --c1x0 4 --w1x0 8.91 --p1x0 7 --n1x1 1 --c1x1 0 --p1x1 0.15 --n1x2 2 --c1x2 3 --w1x2 1.68 --p1x2 10 --s2 1 --n2 1 --n2x0 0 --c2x0 3 --w2x0 6.93 --p2x0 4 --s3 4 --att3 3.71 --n3 2 --n3x0 0 --c3x0 1 --p3x0 0.50 --n3x1 2 --c3x1 5 --p3x1 0.62
     """
+    pass
+
+
+
+
 if __name__ == '__main__':
     print("hello")
